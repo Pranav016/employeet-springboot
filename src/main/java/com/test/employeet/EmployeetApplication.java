3d0887a5eda8180222b834195e9f69aa778cdde5
Pranav Mendiratta <pranavmendi@gmail.com>
2022-09-16 13:44:21 +0530

    Initial commit

---
+++ src/main/java/com/test/employeet/EmployeetApplication.java
@@ -0,0 +1,13 @@
+package com.test.employeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/employeet/EmployeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.employeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
