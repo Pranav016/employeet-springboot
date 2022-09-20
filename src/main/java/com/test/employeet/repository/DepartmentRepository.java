0861a852ab0d066eec16109ceb324d7fa3ecd8fa
Pranav Mendiratta <pranavmendi@gmail.com>
2022-09-20 14:46:23 +0530

    Add department entity and repository

---
+++ src/main/java/com/test/employeet/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.test.employeet.repository;
+
+import com.test.employeet.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
