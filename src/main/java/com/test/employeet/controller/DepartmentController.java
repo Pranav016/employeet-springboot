156bc0efebf8f2ed31629bec9079d8573e89bed2
Pranav Mendiratta <pranavmendi@gmail.com>
2022-09-20 14:47:04 +0530

    Add department controller,dept service interface and service implementation

---
+++ src/main/java/com/test/employeet/controller/DepartmentController.java
@@ -0,0 +1,21 @@
+package com.test.employeet.controller;
+
+import com.test.employeet.entity.Department;
+import com.test.employeet.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @PostMapping("/departments")
+    public Department saveDepartment(@RequestBody Department department){
+        return departmentService.saveDepartment(department);
+
+    }
+}
---
+++ src/main/java/com/test/employeet/service/DepartmentService.java
@@ -0,0 +1,7 @@
+package com.test.employeet.service;
+
+import com.test.employeet.entity.Department;
+
+public interface DepartmentService {
+    public Department saveDepartment(Department department);
+}
