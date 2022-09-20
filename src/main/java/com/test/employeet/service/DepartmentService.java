adb28aec3618424cba9fd14c06274c91ff32948e
Pranav Mendiratta <pranavmendi@gmail.com>
2022-09-20 20:24:55 +0530

    Add service functionalities to departmentServiceImpl.java

--- src/main/java/com/test/employeet/service/DepartmentService.java
+++ src/main/java/com/test/employeet/service/DepartmentService.java
@@ -2,6 +2,16 @@ package com.test.employeet.service;
 
 import com.test.employeet.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
     public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
+
+    public Department fetchDepartmentById(Long departmentId);
+
+    public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartmentById(Long departmentId, Department department);
 }
