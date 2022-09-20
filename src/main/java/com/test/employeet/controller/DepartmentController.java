a2009efadb51191d748c46c2147c0ca7ca4b5041
Pranav Mendiratta <pranavmendi@gmail.com>
2022-09-20 20:23:14 +0530

    Add GET,POST,PUT,DELETE mappings to department controller

--- src/main/java/com/test/employeet/controller/DepartmentController.java
+++ src/main/java/com/test/employeet/controller/DepartmentController.java
@@ -3,9 +3,9 @@ package com.test.employeet.controller;
 import com.test.employeet.entity.Department;
 import com.test.employeet.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -13,9 +13,29 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    @GetMapping("/departments")
+    public List<Department> fetchAllDepartments(){
+        return departmentService.fetchDepartmentList();
+    }
+
     @PostMapping("/departments")
     public Department saveDepartment(@RequestBody Department department){
         return departmentService.saveDepartment(department);
+    }
 
+    @GetMapping("/departments/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
+        return departmentService.fetchDepartmentById(departmentId);
+    }
+
+    @DeleteMapping("/departments/{id}")
+        public String deleteDepartmentById (@PathVariable("id") Long departmentId){
+            departmentService.deleteDepartmentById(departmentId);
+            return "Department deleted successfully";
+    }
+
+    @PutMapping("/departments/{id}")
+    public Department updateDepartmentById(@PathVariable("id") Long departmentId, @RequestBody Department department){
+        return departmentService.updateDepartmentById(departmentId, department);
     }
 }
