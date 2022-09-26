0acf56547cc729aef1e3ee2ae7d91bdca8933f3a
Pranav Mendiratta <pranavmendi@gmail.com>
2022-09-26 14:24:41 +0530

    Fix bug in updateDepartmentById, add java logger

--- src/main/java/com/test/employeet/service/DepartmentServiceImpl.java
+++ src/main/java/com/test/employeet/service/DepartmentServiceImpl.java
@@ -1,8 +1,11 @@
 package com.test.employeet.service;
 
+import com.test.employeet.controller.DepartmentController;
 import com.test.employeet.entity.Department;
 import com.test.employeet.error.DepartmentNotFoundException;
 import com.test.employeet.repository.DepartmentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +15,7 @@ import java.util.Optional;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService{
-
+    private final Logger LOGGER= LoggerFactory.getLogger(DepartmentServiceImpl.class);
     @Autowired
     private DepartmentRepository departmentRepository;
 
@@ -28,11 +31,11 @@ public class DepartmentServiceImpl implements DepartmentService{
 
     @Override
     public Department fetchDepartmentById(Long departmentId) throws DepartmentNotFoundException {
-        Optional<Department> department= departmentRepository.findById(departmentId);
-        if(!department.isPresent()){
-            throw new DepartmentNotFoundException("Department not available");
-        }
-        return department.get();
+            Optional<Department> department = departmentRepository.findById(departmentId);
+            if (!department.isPresent()) {
+                throw new DepartmentNotFoundException("Department not available");
+            }
+            return department.get();
     }
 
     @Override
@@ -49,11 +52,11 @@ public class DepartmentServiceImpl implements DepartmentService{
         }
 
         if(Objects.nonNull(department.getDepartmentCode()) && !"".equalsIgnoreCase(department.getDepartmentCode())){
-            depDB.setDepartmentName(department.getDepartmentCode());
+            depDB.setDepartmentCode(department.getDepartmentCode());
         }
 
         if(Objects.nonNull(department.getDepartmentAddress()) && !"".equalsIgnoreCase(department.getDepartmentAddress())){
-            depDB.setDepartmentName(department.getDepartmentAddress());
+            depDB.setDepartmentAddress(department.getDepartmentAddress());
         }
 
         return departmentRepository.save(depDB);
